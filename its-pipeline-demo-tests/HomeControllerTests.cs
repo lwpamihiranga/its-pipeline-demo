1450b4f8fc7c32fa2795d628e80397dfabfad2c9
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-05-01 11:54:42 +0530

    init unit testing

---
+++ its-pipeline-demo-tests/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using its_pipeline_demo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace its_pipeline_demo_tests
+{
+    public class HomeControllerTests
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        [Fact]
+        public void IndexViewTest()
+        {
+            var homeController = new HomeController(_logger);
+            var result = homeController.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void TestPrivacy()
+        {
+            var homeController = new HomeController(_logger);
+            var result = homeController.Privacy();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
